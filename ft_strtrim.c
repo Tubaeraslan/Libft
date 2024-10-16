774a2d2c46585f2b59cb4556bf35680b7dcd8aba
Tuba Eraslan <tubaeraslan193@gmail.com>
2024-10-16 18:59:08 +0300

    .

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tubaeraslan <tubaeraslan@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 21:25:55 by tubaeraslan       #+#    #+#             */
-/*   Updated: 2024/10/14 21:28:43 by tubaeraslan      ###   ########.fr       */
+/*   Updated: 2024/10/16 18:11:51 by tubaeraslan      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,25 @@ char *ft_strtrim(char const *s1, char const *set){
         s1++;
     }
     len = ft_strlen(s1);
+
+    while (len && s1[len -1] && ft_strchr(set,s1[len-1]) !=0)
+    {
+        len--;
+    }
+    result=(char *)malloc(sizeof(char)*(len +1));
+    if (!result)
+    {
+        return 0;
+    }
+    ft_memcpy(result,s1,len);
+    result[len]='\0';
+    return result;
 }
+
+int main(int argc, char const *argv[])
+{
+    printf("%s \n",ft_strtrim("ahellhoh","a"));
+    return 0;
+}
+
+
