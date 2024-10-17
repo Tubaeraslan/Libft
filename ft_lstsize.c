aa193e0317ba29df2f560ed8165e6df15c8bbbe5
Tuba Eraslan <tubaeraslan193@gmail.com>
2024-10-17 20:46:40 +0300

    ....

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -1,41 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tubaeraslan <tubaeraslan@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/17 19:09:52 by tubaeraslan       #+#    #+#             */
-/*   Updated: 2024/10/17 20:43:11 by tubaeraslan      ###   ########.fr       */
+/*   Created: 2024/10/17 20:34:32 by tubaeraslan       #+#    #+#             */
+/*   Updated: 2024/10/17 20:44:07 by tubaeraslan      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content){
-    t_list *new;
-    new = malloc(sizeof(t_list));
-    if (!new)
+int ft_lstsize(t_list *lst){
+    int i;
+
+    i = 1;
+    if (!lst)
     {
         return 0;
     }
-    new->content=content;
-    new->next=NULL;
-    return new;
+    while (lst)
+    {
+        lst=lst->next;
+        i++;
+    }
+    return i;
 }
 
-/*
 int main(int argc, char const *argv[])
 {
-    t_list *node;
-    char *data = "hello world";
-    node = ft_lstnew(data);
-
-    if (node)
-    {
-        printf("%s \n",(char *)node->content);
-    }
-    
+    char data[]="hello world";
+    t_list *node=ft_lstnew(data);
+    printf("%d \n",ft_lstsize(node));
     return 0;
 }
-*/
